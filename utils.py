adf57801292101e559eecbbbb23d78192da0c9c2
Alextaylorvhjnf <alextaylor98ap@gmail.com>
2025-11-29 18:00:49 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from config import FORCE_SUB_CHANNEL
+from config import FORCE_SUB_CHANNEL, BOT_USERNAME
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -9,18 +9,18 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def create_start_link(film_code):
-    return f"https://t.me/Senderpfilesbot?start={film_code}"
+    return f"https://t.me/{BOT_USERNAME}?start={film_code}"
 
 def get_join_channel_keyboard():
     keyboard = [
-        [InlineKeyboardButton("عضویت در کانال 📢", url=f"https://t.me/{FORCE_SUB_CHANNEL[1:]}")],
-        [InlineKeyboardButton("عضو شدم ✔", callback_data="check_join")]
+        [InlineKeyboardButton("📢 عضویت در کانال", url=f"https://t.me/{FORCE_SUB_CHANNEL[1:]}")],
+        [InlineKeyboardButton("✅ عضو شدم", callback_data="check_join")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
 def get_main_keyboard():
     keyboard = [
-        [InlineKeyboardButton("راهنما 📖", callback_data="help")],
-        [InlineKeyboardButton("لیست فیلم‌ها 🎬", callback_data="list_films")]
+        [InlineKeyboardButton("📖 راهنما", callback_data="help")],
+        [InlineKeyboardButton("🎬 لیست فیلم‌ها", callback_data="list_films")]
     ]
     return InlineKeyboardMarkup(keyboard)
